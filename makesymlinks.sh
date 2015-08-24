a0bb25952a1ca571dc72b048264cfb63a89c70eb
Dmitry Nets <dmitrynets@gmail.com>
2015-08-24 09:10:15 +0300

    removed taskrc

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -12,7 +12,7 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 
 # list of files/folders to symlink in homedir
-files="vimrc zshrc taskrc tmuxrc"
+files="vimrc zshrc taskrc tmux.conf"
 ##########
 
 # Create dotfiles_old in homedir
