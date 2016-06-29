9d72670bedd60538f25303635842fa3a38ae9a8f
Dmitry Nets <dmitrynets@gmail.com>
2016-06-29 23:52:03 +0300

    ADD install script for molokai vim colorscheme

--- vim-install-molokai-colors.sh
+++ vim-install-molokai-colors.sh
@@ -3,9 +3,9 @@
 # install custom powerline theme dnets-theme
 
 # oh-my-zsh folder
-omzfolder=~/.oh-my-zsh
+vimfolder=~/.vim
 
 echo "Installing dnets.zsh-theme"
-cp -r ./dnets.zsh-theme $omzfolder/custom/themes/
+cp -r ./molokai.vim $vimfolder/colors/
 
 echo "All done. Enjoy the best theme ever!"
